e259bc58927b151a55154f0db20818c3ca3fba91
Nuo Chen <nchen67@wisc.edu>
2020-05-30 16:05:39 -0500

    fixed standard atmoshpere bugs:1)unit confusion 2) increase and decrease sequences are both acceptable now; Add a test case for sa module

--- standard_atmosphere.py
+++ standard_atmosphere.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+
+#https://www.numeric-gmbh.ch/posts/standard-atmosphere-calculations.html
+
 # Set constants
 hPa = 0.01
 g = 9.8066         # m/s2
@@ -36,14 +39,15 @@ def gravity_adjust(Z):
 
     return g2
 
-def cal_zttheta(p, unit='Pa'):
+def cal_zttheta(p):
     """
     Calculate standard temperature, altitude, and potential temperature from standard pressure.
 
     Parameters
     ----------
     p : array_like
-        Standard pressure. Unit default to 'Pa', but 'hPa' is also acceptable
+        Standard pressure. 
+        !Caution: input pressure should increase in sequence and with unit of 'Pa'
 
     Returns
     -------
@@ -59,24 +63,20 @@ def cal_zttheta(p, unit='Pa'):
     Z = np.zeros(nlevels)
     T = np.zeros(nlevels)
     theta = np.zeros(nlevels)
-    if unit == 'hPa' or unit == 'mb':
-        P_tropopause = 226.32 #hPa
-        p00 = 1013.25
-    elif unit == 'Pa':
-        P_tropopause = 22632 #hPa
-        p00 = 101325
-    tropopause_idx = np.where(p>=P_tropopause)[0][-1] + 1
-
+    #if unit == 'Pa':
+    P_tropopause = 22632 #Pa
+    tropopause_idx = np.where(p>=P_tropopause)[0][0]
+    
     # calculate variables below the tropopause
-    Z[:tropopause_idx] = (288.15/gamma) * ( 1. - (1013.25/p[:tropopause_idx])**alpha)
-    T[:tropopause_idx] = 288.15 - 0.0065*Z[:tropopause_idx]
-    theta[:tropopause_idx] = T[:tropopause_idx] * (p00/p[:tropopause_idx])**(R/cp)
+    Z[tropopause_idx:] = (288.15/gamma) * ( 1. - (101325/p[tropopause_idx:])**alpha)
+    T[tropopause_idx:] = 288.15 - 0.0065*Z[tropopause_idx:]
+    theta[tropopause_idx:] = T[tropopause_idx:] * (p00/p[tropopause_idx:])**(R/cp)
 
     # calculate variables above the tropopause
-    T[tropopause_idx:] = 216.65
-    Z[tropopause_idx:] = (11.e3 + beta * np.log(p[tropopause_idx:] / 226.32))
+    T[:tropopause_idx] = 216.65
+    Z[:tropopause_idx] = 11.e3 + beta * np.log(p[:tropopause_idx] / 22632)
     theta[:tropopause_idx] = T[:tropopause_idx] * (p00/p[:tropopause_idx])**(R/cp)
-
+    
     return T, Z, theta
 
 def diff(a, i=1):
@@ -99,19 +99,18 @@ def diff(a, i=1):
     n = len(a)
     return a[i:]-a[:n-i]
 
-def atmos_structure(p):
+def atmos_structure(p, unit='Pa'):
     """
     Calculate geopotential height, temperature, and stratification parameter in standard atmosphere.
 
     Parameters
     ----------
-    nlevs : int
-        Number of levels for the standard atmosphere.
-        Default to 19 layers.
-    pb : int
-        Pressure at bottom atmosphere level.
-    pt : int
-        Pressure at top atmosphere level.
+    p : array_like
+        Standard pressure. Unit default to 'Pa', but 'hPa' is also acceptable.
+        The return values should be in the same sequence as p (i.e. increase or decrease).
+    unit : str
+        Default to 'Pa', but 'hPa' and 'mb' are also acceptable.
+        If you are using hPa, but did not provide the unit argument, this function will automaticly convert it into Pa  when the largest pressure values < 2000.
 
     Returns
     -------
@@ -125,6 +124,18 @@ def atmos_structure(p):
         Stratification parameter for the given standard atmosphere.
         len(S) = nlevs - 1
     """
+    ### reverse the pressure if it is an increasing sequence
+    if p[-1]<p[0]:
+        p = p[::-1]
+        reverse_flag = True
+    else:
+        reverse_flag = False
+    
+    ### Convert into Pa if in hPa
+    if p.max() < 2000 or unit=='hPa' or unit=='mb':
+        p = p*100
+    
+    # Begin calculation
     R = 287.05
     
     nlevs = len(p)
@@ -134,14 +145,23 @@ def atmos_structure(p):
     p_half = (p[1:] + p[:-1])/2
 
     # Calculate temperature, altitude, potential temperature structure.
-    T, Z, theta = cal_zttheta(p, unit='Pa')
-    T_half, Z_half, theta_half = cal_zttheta(p_half, unit='Pa')
-    g2 = gravity_adjust(Z)
+    T, Z, theta = cal_zttheta(p)
+    T_half, Z_half, theta_half = cal_zttheta(p_half)
 
     # Calculate stratification parameter.
     dthetadp = np.zeros(nlevels_half)
-    dthetadp[0] = (theta[1] - theta[0])/(1.e2*(p[1]-p[0]))
-    dthetadp[1:] = diff(theta,i=2) / (1.e2 * diff(p, i=2))
+    dthetadp[0] = (theta[1] - theta[0])/(p[1]-p[0])
+    dthetadp[1:] = diff(theta,i=2) / (diff(p, i=2))
     S_half = - R * (T_half / theta_half) * dthetadp / p_half  # p[:-1]? or p_half
+    
+    # Reverse back the return values
+    if reverse_flag == True:
+        Z = Z[::-1]
+        T = T[::-1]
+        S_half = S_half[::-1]
+        
+    g2 = gravity_adjust(Z)
 
     return Z, g2*Z, T, S_half
+
+
